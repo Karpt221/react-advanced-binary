733d4dba68947cd8eb184bbddc29a9338ddb2b4a
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-12 12:17:08 +0300

    * make required optional

--- src/components/primitives/input/Input.tsx
+++ src/components/primitives/input/Input.tsx
@@ -1,10 +1,12 @@
 import styles from './Input.module.css';
 
-function Input({ heading, type, other }: { heading: string; type: string; other: Record<string, string | number> }) {
+type InputAttributes = { heading: string; type: string; other: Record<string, string | number | boolean> };
+
+function Input({ heading, type, other }: InputAttributes) {
     return (
         <label className={styles.input}>
             <span className={styles.input__heading}>{heading}</span>
-            <input name={type} type={type} required {...other} />
+            <input name={type} type={type} {...other} />
         </label>
     );
 }
