ec479dfc1cf08fe95950718505c2c779225b5e94
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-13 13:48:04 +0300

    + app routes enum

--- src/enums/enums.ts
+++ src/enums/enums.ts
@@ -1,2 +1,3 @@
 export { LEVELS } from './levels/levels.enum';
 export { DURATIONS } from './durations/durations.enum';
+export { APP_ROUTES } from './routes/routes.enum';
---
+++ src/enums/routes/routes.enum.ts
@@ -0,0 +1,9 @@
+const APP_ROUTES = {
+    MAIN: '/',
+    SIGN_IN: '/sign-in',
+    SIGN_UP: '/sign-up',
+    BOOKINGS: '/bookings',
+    TRIP: '/trip/:tripId',
+} as const;
+
+export { APP_ROUTES };
