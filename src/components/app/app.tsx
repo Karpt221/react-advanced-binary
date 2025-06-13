377e2be8c75ff3e0c50222c434be7cbbe011b1e1
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-13 13:47:09 +0300

    * move router provider to main file

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,14 @@
-import RouterProvider from '../router-provider/router-provider';
 import Header from '../layout/header/header';
 import Footer from '../layout/footer/footer';
 import Main from '../layout/main/main-layout';
-import appRoutes from '../routes/routes';
+import { Outlet } from 'react-router';
 
 function App() {
     return (
         <>
             <Header />
             <Main>
-                <RouterProvider routes={appRoutes} />
+                 <Outlet />
             </Main>
             <Footer />
         </>
