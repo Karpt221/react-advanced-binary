42bf7c3b0a965aba18b9e8ebca0f730ae8b92c5b
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-13 14:06:44 +0300

    + header routing

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,21 +1,27 @@
 import styles from './header.module.css';
 import briefcaseIcon from '../../../assets/images/briefcase.svg';
 import ProfileNav from './profile-nav/profile-nav';
+import { Link } from 'react-router';
+import { APP_ROUTES } from '../../../enums/enums';
 
 function Header() {
     return (
         <header className={styles.header}>
             <div className={styles['header__inner']}>
-                <a data-test-id="header-logo" href="" className={styles.header__logo}>
+                <Link to={APP_ROUTES.MAIN} data-test-id="header-logo" className={styles.header__logo}>
                     Travel App
-                </a>
+                </Link>
                 <nav data-test-id="header-nav" className={styles.header__nav}>
                     <ul className={styles['nav-header__list']}>
                         <li className={styles['nav-header__item']} title="Bookings">
-                            <a data-test-id="header-bookings-link" href="" className={styles['nav-header__inner']}>
+                            <Link
+                                to={APP_ROUTES.BOOKINGS}
+                                data-test-id="header-bookings-link"
+                                className={styles['nav-header__inner']}
+                            >
                                 <span className="visually-hidden">Bookings</span>
                                 <img src={briefcaseIcon} alt="bookings" />
-                            </a>
+                            </Link>
                         </li>
                         <li className={styles['nav-header__item']} title="Profile">
                             <ProfileNav className={styles['nav-header__inner']} />
