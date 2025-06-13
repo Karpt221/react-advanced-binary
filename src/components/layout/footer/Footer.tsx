a1d8df3b340631531b483c00e1086dc85b3f1ce9
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-13 10:56:02 +0300

    + footer component

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import styles from './Footer.module.css';
+import heartIcon from '../../../assets/images/heart.svg';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <span className={styles.footer__text}>
+                © 2025, from
+                <a className={styles.footer__link} href="https://binary-studio.com">
+                    binary studio
+                </a>
+                with
+                <img className={styles.footer__icon} src={heartIcon} alt="heart" />
+            </span>
+        </footer>
+    );
+}
+
+export default Footer;
