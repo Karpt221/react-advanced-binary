d1619b3015ca792677f7ea2efc60d15e97543050
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-14 10:54:38 +0300

    - const assertion

--- src/enums/durations/durations.enum.ts
+++ src/enums/durations/durations.enum.ts
@@ -1,7 +1,7 @@
 const DURATIONS = [
-    { value: '0_x_5', text: '\u003C 5 days' } as const,
-    { value: '0_x_10', text: '\u003C 10 days' } as const,
-    { value: '10', text: '\u2265 10 days' } as const,
+    { value: '0_x_5', text: '\u003C 5 days' },
+    { value: '0_x_10', text: '\u003C 10 days' },
+    { value: '10', text: '\u2265 10 days' },
 ] as const;
 
 export { DURATIONS };
