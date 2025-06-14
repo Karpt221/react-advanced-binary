0162141d15974f76c146c6d44ac174607db88615
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-14 22:33:30 +0300

    + trip id param missing handling

--- src/components/routes/loaders/trip-page/trip-page.loader.ts
+++ src/components/routes/loaders/trip-page/trip-page.loader.ts
@@ -1,7 +1,14 @@
 import { tripsRepository } from '~/repositories/trips.repository';
 
-function tripPageLoader({ params }: { params: { tripId: string } }) {
-    const tripData = tripsRepository.getById(params.tripId);
+function tripPageLoader({ params }: { params: { tripId?: string } }) {
+    const tripId = params.tripId;
+
+    if (!tripId) {
+        throw new Error('Trip ID is required.');
+    }
+
+    const tripData = tripsRepository.getById(tripId);
+
     return { tripData };
 }
 
