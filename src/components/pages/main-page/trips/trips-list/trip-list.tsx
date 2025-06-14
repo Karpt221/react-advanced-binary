e30250812ca8a722f887f42cb9c3b9349c4d29d0
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-14 12:46:46 +0300

    + trip list component

---
+++ src/components/pages/main-page/trips/trips-list/trip-list.tsx
@@ -0,0 +1,19 @@
+import type { Trip } from '~/types/types';
+import styles from './trip-list.module.css';
+import TripCard from '../trip-card/trip-card';
+
+type TripListProps = Readonly<{
+    tripsData: Trip[];
+}>;
+
+function TripList({ tripsData }: TripListProps) {
+    return (
+        <ul className={styles['trip-list']}>
+            {tripsData.map((trip, index) => {
+                return <TripCard key={index} tripData={trip} />;
+            })}
+        </ul>
+    );
+}
+
+export default TripList;
