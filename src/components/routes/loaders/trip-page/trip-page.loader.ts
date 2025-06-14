ca3a25cf69a95f94c3e624fb15f58f525531c3ac
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-14 18:06:42 +0300

    + trip page component

--- src/components/routes/loaders/trip-page/trip-page.loader.ts
+++ src/components/routes/loaders/trip-page/trip-page.loader.ts
@@ -1,8 +1,8 @@
 import { tripsRepository } from '~/repositories/trips.repository';
 
 function tripPageLoader({ params }: { params: { tripId: string } }) {
-    const tripById = tripsRepository.getById(params.tripId);
-    return { tripById };
+    const tripData = tripsRepository.getById(params.tripId);
+    return { tripData };
 }
 
 export { tripPageLoader };
