d04cf9d3b87da3d773b1ded73ab607e109843fcc
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-14 12:41:52 +0300

    + additional class names

--- src/components/primitives/input/input.tsx
+++ src/components/primitives/input/input.tsx
@@ -4,14 +4,15 @@ type InputProps = Readonly<{
     heading: string;
     type: string;
     dataTestId: string;
-    other: Readonly<Record<string, string | number | boolean>>;
-    className?: string;
+    other?: Readonly<Record<string, string | number | boolean>>;
+    labelClassName?: string;
+    headingClassName?: string;
 }>;
 
-function Input({ dataTestId, heading, type, className = '', other }: InputProps) {
+function Input({ dataTestId, heading, type, labelClassName = '', headingClassName = '', other }: InputProps) {
     return (
-        <label className={`${styles.input} ${className}`}>
-            <span className={styles.input__heading}>{heading}</span>
+        <label className={`${styles.input} ${labelClassName}`}>
+            <span className={`${styles.input__heading} ${headingClassName}`}>{heading}</span>
             <input data-test-id={dataTestId} name={type} type={type} {...other} />
         </label>
     );
