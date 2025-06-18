170761732db70933962f728fe9fb2c90bcc52530
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-19 01:11:10 +0300

    + trips and bookings api

--- src/services/travel-api/auth/auth.api.ts
+++ src/services/travel-api/auth/auth.api.ts
@@ -2,26 +2,23 @@ import type { UserDto } from '~/types/travel-api/user-dto.type';
 import type { SignInRequestDto, SignInResponseDto, SignUpRequestDto, SignUpResponseDto } from '~/types/types';
 import { httpApi } from '../../http/http.api';
 import { AUTH_API_PATH, CONTENT_TYPE, HTTP_METHOD } from '~/enums/enums';
+import { type ApiConstructor } from '~/types/types';
 
-type Constructor = {
-    apiUrl: string;
-    authPath: Record<keyof typeof AUTH_API_PATH, string>;
-    http: typeof httpApi;
-};
+type AuthApiConstructor = ApiConstructor<typeof AUTH_API_PATH, typeof httpApi>;
 
 class AuthApi {
-    #apiUrl: Constructor['apiUrl'];
-    #authPath: Constructor['authPath'];
-    #http: Constructor['http'];
+    #baseUrl: AuthApiConstructor['baseUrl'];
+    #paths: AuthApiConstructor['paths'];
+    #http: AuthApiConstructor['http'];
 
-    constructor({ apiUrl, authPath, http }: Constructor) {
-        this.#apiUrl = apiUrl;
-        this.#authPath = authPath;
+    constructor({ baseUrl, paths, http }: AuthApiConstructor) {
+        this.#baseUrl = baseUrl;
+        this.#paths = paths;
         this.#http = http;
     }
 
     public async signIn(payload: SignInRequestDto): Promise<SignInResponseDto> {
-        return this.#http.fetch(this.#getUrl(this.#authPath.SIGN_IN), {
+        return this.#http.fetch(this.#getUrl(this.#paths.SIGN_IN), {
             method: HTTP_METHOD.POST,
             contentType: CONTENT_TYPE.JSON,
             payload: JSON.stringify(payload),
@@ -30,7 +27,7 @@ class AuthApi {
     }
 
     public signUp(payload: SignUpRequestDto): Promise<SignUpResponseDto> {
-        return this.#http.fetch(this.#getUrl(this.#authPath.SIGN_UP), {
+        return this.#http.fetch(this.#getUrl(this.#paths.SIGN_UP), {
             method: HTTP_METHOD.POST,
             contentType: CONTENT_TYPE.JSON,
             payload: JSON.stringify(payload),
@@ -39,14 +36,14 @@ class AuthApi {
     }
 
     public authenticate(): Promise<UserDto> {
-        return this.#http.fetch(this.#getUrl(this.#authPath.AUTHENTICATED_USER), {
+        return this.#http.fetch(this.#getUrl(this.#paths.AUTHENTICATED_USER), {
             method: HTTP_METHOD.GET,
             hasAuth: true,
         });
     }
 
-    #getUrl(path: string) {
-        return `${this.#apiUrl}${this.#authPath.BASE}${path}`;
+    #getUrl(path: string = '') {
+        return `${this.#baseUrl}${this.#paths.BASE}${path}`;
     }
 }
 
