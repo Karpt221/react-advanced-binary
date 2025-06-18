818549c3281e226e59544dc866939b070004c05e
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-18 14:48:06 +0300

    * display header nav according to location

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,9 +1,18 @@
 import styles from './header.module.css';
-import { Link } from 'react-router';
-import { APP_ROUTES } from '~/enums/enums';
+import { Link, useLocation } from 'react-router';
+import { APP_ROUTES, DATA_STATUS } from '~/enums/enums';
 import HeaderNav from './header-nav/header-nav';
+import { useAppSelector } from '~/services/store/hooks';
 
-function Header({ isNavHidden = false }: { isNavHidden?: boolean }) {
+function Header() {
+    const location = useLocation();
+    const path = location.pathname;
+
+    const authenticationStatus = useAppSelector((state) => state.auth.authenticationStatus);
+
+    const isNavHidden =
+        authenticationStatus !== DATA_STATUS.FULFILLED || path === APP_ROUTES.SIGN_IN || path === APP_ROUTES.SIGN_UP;
+
     return (
         <header className={styles.header}>
             <div className={styles['header__inner']}>
