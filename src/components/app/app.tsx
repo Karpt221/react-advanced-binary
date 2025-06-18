3b1c764eb54b8719127e4abe725cedc1f07aaa62
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-19 01:30:51 +0300

    - bookings state from App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,31 +1,12 @@
-import { type BookingResponseDto } from '~/types/types';
-import importedBookings from '~/assets/data/bookings.json';
-import { useState } from 'react';
 import RouterProvider from '../router-provider/router-provider';
 import { APP_ROUTES } from '~/enums/enums';
-import { mainPageLoader, tripPageLoader, unknownRouteLoader } from '../routes/loaders/loaders';
+import { tripPageLoader, unknownRouteLoader } from '../routes/loaders/loaders';
 import { BookingsPage, MainPage, SignIn, SignUp, TripPage } from '../pages/pages';
 import Navigator from '../navigator/navigator';
 import { ToastContainer } from 'react-toastify';
 import ProtectedRoute from '../protected-route/protected-route';
 
-const bookingsData = importedBookings as BookingResponseDto[];
-
 function App() {
-    const [bookings, setBookings] = useState<BookingResponseDto[]>(bookingsData);
-
-    const addBooking = (newBooking: Omit<BookingResponseDto, 'id' | 'userId' | 'createdAt'>) => {
-        const id = crypto.randomUUID() as string;
-        const userId = crypto.randomUUID() as string;
-        const createdAt = new Date().toString();
-        const bookingWithID: BookingResponseDto = { ...newBooking, id, userId, createdAt };
-        setBookings((prevBookings) => [...prevBookings, bookingWithID]);
-    };
-
-    const removeBooking = (bookingId: string) => {
-        setBookings((prevBookings) => [...prevBookings.filter((booking) => booking.id !== bookingId)]);
-    };
-
     return (
         <>
             <ToastContainer className="notification" toastClassName="notification" />
@@ -41,7 +22,6 @@ function App() {
                                         <MainPage />
                                     </ProtectedRoute>
                                 ),
-                                loader: mainPageLoader,
                             },
                             {
                                 path: APP_ROUTES.SIGN_IN,
@@ -55,7 +35,7 @@ function App() {
                                 path: APP_ROUTES.BOOKINGS,
                                 element: (
                                     <ProtectedRoute>
-                                        <BookingsPage bookings={bookings} onRemove={removeBooking} />
+                                        <BookingsPage />
                                     </ProtectedRoute>
                                 ),
                             },
@@ -63,7 +43,7 @@ function App() {
                                 path: `${APP_ROUTES.TRIP}/:tripId`,
                                 element: (
                                     <ProtectedRoute>
-                                        <TripPage onAddBooking={addBooking} />
+                                        <TripPage />
                                     </ProtectedRoute>
                                 ),
                                 loader: tripPageLoader,
