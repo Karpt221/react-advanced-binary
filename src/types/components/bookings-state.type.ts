821bd0eb007f8b6dcede666d9e5f5e04289fe10e
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-19 01:31:44 +0300

    - bookings state type

--- src/types/components/bookings-state.type.ts
+++
@@ -1,9 +0,0 @@
-import type { BookingResponseDto } from '../types';
-
-type BookingsState = {
-    bookings: BookingResponseDto[];
-    addBooking: (newBooking: Omit<BookingResponseDto, 'id' | 'userId' | 'createdAt'>) => void;
-    removeBooking: (bookingId: string) => void;
-};
-
-export { type BookingsState };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,5 @@
 export { type Level } from './components/level.type';
 export { type Duration } from './components/duration.type';
-export { type BookingsState } from './components/bookings-state.type';
 export { type TripResponseDto } from './travel-api/trip-response-dto.type';
 export { type BookingResponseDto } from './travel-api/booking-response-dto.type';
 export { type BookingRequestDTO } from './travel-api/booking-request-dto.type';
