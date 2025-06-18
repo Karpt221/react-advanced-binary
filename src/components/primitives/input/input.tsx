0806ae5b3213c56d47857101beb13dbbdfc99e87
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-18 14:27:38 +0300

    + name prop to input component

--- src/components/primitives/input/input.tsx
+++ src/components/primitives/input/input.tsx
@@ -3,6 +3,7 @@ import styles from './input.module.css';
 type InputProps = Readonly<{
     heading: string;
     type: string;
+    name?: string;
     dataTestId: string;
     other?: Readonly<Record<string, string | number | boolean>>;
     className?: string;
@@ -10,12 +11,21 @@ type InputProps = Readonly<{
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }>;
 
-function Input({ onChange, dataTestId, heading, type, className = '', isHeadingHidden = false, other }: InputProps) {
+function Input({
+    onChange,
+    dataTestId,
+    heading,
+    type,
+    name,
+    className = '',
+    isHeadingHidden = false,
+    other,
+}: InputProps) {
     const hiddenValue = isHeadingHidden ? 'visually-hidden' : '';
     return (
         <label className={`${styles.input} ${className}`}>
             <span className={`${styles.input__heading} ${hiddenValue}`}>{heading}</span>
-            <input onChange={onChange} data-test-id={dataTestId} name={type} type={type} {...other} />
+            <input onChange={onChange} data-test-id={dataTestId} name={name || type} type={type} {...other} />
         </label>
     );
 }
