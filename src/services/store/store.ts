4ab160d18a8bf8cd16dbd66d94c040f187f0ca2e
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2025-06-18 14:38:02 +0300

    + unauthorized error listener

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,19 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { reducer as authRducer } from './auth/auth';
+import { configureStore, createListenerMiddleware, isRejected, type TypedStartListening } from '@reduxjs/toolkit';
+import { reducer as authRducer, actions as authActions } from './auth/auth';
 import { travelApi } from '~/services/travel-api/travel.api';
+import { reducer as navReducer } from './navigation/navigation-slice';
+import type { AppDispatch, RootState } from '~/types/store/store.type';
 
 const extraArgument = { travelApi };
+
+const listenerMiddleware = createListenerMiddleware();
+
+type AppStartListening = TypedStartListening<RootState, AppDispatch>;
+const startAppListening = listenerMiddleware.startListening as AppStartListening;
+
+startAppListening({
+    matcher: isRejected,
+    effect: async (action, listenerApi) => {
+        if (action.payload && (action.payload as { isUnauthorized?: boolean }).isUnauthorized) {
+            await listenerApi.dispatch(authActions.signOutAction());
+        }
+    },
+});
 
 const store = configureStore({
     reducer: {
         auth: authRducer,
+        nav: navReducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware({
             thunk: {
                 extraArgument,
             },
-        });
+        }).prepend(listenerMiddleware.middleware);
     },
 });
 
